4254b645c142b9c342d6304dae44722bf3b430a4
dell <dell@DESKTOP-N4E76DF>
2021-07-26 12:43:12 +0530

    push

---
+++ Project_1/src/pkg1/LoopAss1.java
@@ -0,0 +1,21 @@
+package pkg1;
+
+public class LoopAss1 {
+
+	public static void main(String[] args)
+	{
+		for (int i=0; i<=5; i++)
+		{
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(" * ");
+			}
+			System.out.println();
+		}
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
